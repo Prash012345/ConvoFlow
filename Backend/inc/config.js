ded8cf964fd46592600e595052eb3748c3afe2b5
Prash012345 <130853468+Prash012345@users.noreply.github.com>
2025-08-20 17:58:59 +0530

    Update config.js

--- Backend/inc/config.js
+++ Backend/inc/config.js
@@ -1,4 +1,4 @@
-const connStr = "mongodb+srv://kareenajaiswal123:jaiswalk@mycluster.77ekz.mongodb.net/Chat-App";
-const jwtSecret = "karu1234"
+const connStr = "";
+const jwtSecret = "prash"
 let tokenBlacklist = [];
 module.exports = {connStr,jwtSecret,tokenBlacklist};
